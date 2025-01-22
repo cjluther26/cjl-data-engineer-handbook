89724382f8659630888a9057fe789e429ff9c578
cjluther26 <cjluther26@yahoo.com>
2025-01-22 16:05:21 -0500

    creating doc for spark lab

---
+++ bootcamp/materials/3-spark-fundamentals/cjl_notes/lab_1.sql
@@ -0,0 +1,7 @@
+/*
+SETTING UP SPARK
+In the `data-engineer-handbook` repo, cd'd into the `3-spark-fundamentals` directory
+and ran `docker compose up -d`.
+
+Then, I simply went to localhost:8888 in my browser to access the Spark jupyter notebook!
+*/
