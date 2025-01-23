2f9c8b041e9c7003b7b11f12e1d9874a79cc6e68
cjluther26 <cjluther26@yahoo.com>
2025-01-23 11:54:15 -0500

    removing unused notes

--- bootcamp/materials/3-spark-fundamentals/cjl_notes/lab_1.sql
+++
@@ -1,7 +0,0 @@
-/*
-SETTING UP SPARK
-In the `data-engineer-handbook` repo, cd'd into the `3-spark-fundamentals` directory
-and ran `docker compose up -d`.
-
-Then, I simply went to localhost:8888 in my browser to access the Spark jupyter notebook!
-*/
